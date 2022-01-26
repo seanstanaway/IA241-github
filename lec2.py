c3b9d9680eb40210dcd96924ee6adad611095075
EC2 Default User <ec2-user@ip-172-31-37-54.ec2.internal>
2022-01-26 21:40:48 +0000

    lec2

---
+++ lec2.py
@@ -0,0 +1,34 @@
+"""
+this is a regional comment
+"""
+
+#print('hello world') # this is a line comment
+
+#print( type(123) )
+
+#print ( type (123.) )
+
+#print( type('123') )
+
+#print( "hello world" )
+#print( 'hello world' )
+
+#print ( "Tom's cat" )
+
+#print(2.**3)
+
+my_int = 123
+
+print(my_int)
+
+my_int = 321
+
+print(my_int)
+
+my_float = 123.
+
+my_str = 'hello world'
+
+print(my_float + 1)
+
+print(my_str.upper ())
